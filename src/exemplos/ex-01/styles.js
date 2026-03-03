a64966c8ab1b89bce2c7a0ebfbc75ca58a79e876
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-03 20:25:13 -0300

    Initial commit

---
+++ src/exemplos/ex-01/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ffff00', 
+        // margin: 20, 
+        padding: 20, 
+        flex: 1, 
+        borderRadius: 20,         
+    },
+    titulo: {
+        fontSize: 30,
+        width: '100%', 
+        textAlign: 'center', 
+        fontWeight: 'bold', 
+        marginBottom: 50,
+    }, 
+    texto: {
+        fontSize: 18,
+    },  
+});
+
+export default styles;
