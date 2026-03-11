d14ab40d48045fed029d51a2121672b090580873
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-10 22:26:43 -0300

    exemplo para atividade

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
 import Exemplo01 from './src/exemplos/ex-01';
+import Exemplo02 from './src/exemplos/ex-02';
 
 import Atividade01 from './src/atividades/atv-01';
+import Atividade02 from './src/atividades/atv-02';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade01 />
+      <Atividade02 />
       <StatusBar style="auto" />
     </View>
   );
