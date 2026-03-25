be78ad04e9d697d9c1b4dfbfb2c332162a309d4b
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-03-24 22:15:25 -0300

    aula 24-03

--- src/revisao/rev-atv-02/index.js
+++ src/revisao/rev-atv-02/index.js
@@ -2,42 +2,22 @@ import { View, Text, Image } from 'react-native';
 
 import styles from './styles';
 
-import amarela from '../../../assets/brasilAmarela.png';
-import azul from '../../../assets/brasilAzul.jpg';
-import branco from '../../../assets/brasilBranco.png';
-import preta from '../../../assets/brasilPreta.jpg';
-
 import Card from './card';
+import mockup from './mockup';
 
 function RevisaoAtv02() {
     return (
         <View style={styles.container}>
             <Text style={styles.titulo}>Revisao Atividade 2</Text>
 
-            <Card
-                titulo={'Camiseta Amarela'}
-                descricao={'Camisa principal da seleção.'}
-                preco={'R$ 499,00'} 
-                img={amarela}
-            />
-            <Card
-                titulo={'Camiseta Azul'}
-                descricao={'Camisa número dois.'}
-                preco={'R$ 499,00'}
-                img={azul}
-            />
-            <Card
-                titulo={'Camiseta Preta'}
-                descricao={'Camisa homenagem.'}
-                preco={'R$ 399,00'}
-                img={preta}
-            />
-            <Card
-                titulo={'Camiseta Branca'}
-                descricao={'Camisa vintage.'}
-                preco={'R$ 300,00'}
-                img={branco}
-            />
+            {
+                mockup.map(item => <Card
+                titulo={item.titulo}
+                descricao={item.descricao}
+                preco={item.preco} 
+                img={item.img}
+            />)
+            }                    
 
         </View>
     );
