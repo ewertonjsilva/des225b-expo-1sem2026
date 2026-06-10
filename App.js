527584bb0ee8393b401d65e4db7ea74ec17be222
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2026-06-09 22:46:24 -0300

    aula 09-06

--- App.js
+++ App.js
@@ -8,14 +8,16 @@ import Exemplo04 from './src/exemplos/ex04';
 import Exemplo05 from './src/exemplos/ex05';
 import Exemplo06 from './src/exemplos/ex06';
 import Exemplo07 from './src/exemplos/ex07'; 
-import Exemplo08 from './src/exemplos/ex08';
+import Exemplo08 from './src/exemplos/ex08'; 
+import Exemplo09 from './src/exemplos/ex09';
 
 import Atividade01 from './src/atividades/atv01';
 import Atividade02 from './src/atividades/atv02'; 
 import Atividade03 from './src/atividades/atv03'; 
 import Atividade04 from './src/atividades/atv04'; 
 import Atividade05 from './src/atividades/atv05'; 
-import Atividade06 from './src/atividades/atv06';
+import Atividade06 from './src/atividades/atv06'; 
+import Atividade09 from './src/atividades/atv09';
 
 import RevisaoAtv02 from './src/revisao/rev-atv-02';
 import Exemplo03_passos from './src/revisao/ex03'; 
@@ -24,7 +26,7 @@ import RevAtv04 from './src/revisao/rev-atv-04';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Exemplo08 />
+      <Atividade09 />
       <StatusBar style="auto" />
     </View>
   );
